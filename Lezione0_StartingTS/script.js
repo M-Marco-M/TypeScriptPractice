a5bc696d3b568010a4a569d55a148a7782172eda
Marco <160071349+M-Marco-M@users.noreply.github.com.>
2024-10-04 14:55:31 +0200

    Cambia Lezione1 in Lezione0

--- Lezione0_StartingTS/script.js
+++ Lezione0_StartingTS/script.js
@@ -1,5 +1,5 @@
 "use strict";
-//Lezione 1: StartingTS
+//Lezione 0: StartingTS
 let a = "marco";
 console.log(a);
 a = "5";
@@ -9,31 +9,31 @@ console.log(b);
 let c = "mario";
 //Dichiaro marco di tipo Person
 const marco = {
-    nome: "Marco",
-    cognome: "MaMa",
-    eta: 26,
+  nome: "Marco",
+  cognome: "MaMa",
+  eta: 26,
 };
 //Adesso dichiaro luigi senza assegnare tipo
 const luigi = {
-    nome: "Luigi",
-    cognome: "LuLu",
-    eta: 28,
+  nome: "Luigi",
+  cognome: "LuLu",
+  eta: 28,
 };
 //La funzione printPerson vuole in ingresso un oggetto di tipo Person
 const printPerson = function (person) {
-    console.log(person);
-    console.log(person.cognome);
+  console.log(person);
+  console.log(person.cognome);
 };
 //richiamo la funzione con marco, di tipo Person e con luigi, che non è esplicitamente di tipo person
 printPerson(marco);
 printPerson(luigi);
 //dichiaro fra98, di tipo user
 const francesco = {
-    nome: "Francesco",
-    cognome: "FraFra",
-    eta: 31,
-    username: "fra98",
-    annoIscrizione: 2010,
+  nome: "Francesco",
+  cognome: "FraFra",
+  eta: 31,
+  username: "fra98",
+  annoIscrizione: 2010,
 };
 printPerson(francesco);
 //La funzione printPerson accetta l'oggetto francesco di tipo utente
